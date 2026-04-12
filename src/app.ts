6608feb9424a31e6e502d205f50bd9648b087eb6
Vlad <vlad.ivashko72@gmail.com>
2026-04-12 20:12:34 +0300

    chore: update message from health

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,11 @@ app.use(cors({ origin: config.clientUrl, credentials: true }));
 
 // health check
 app.get('/health', (_, res) =>
-  res.json({ status: 'ok', version: consts.API_VERSION, message: 'Test message -> check ' }),
+  res.json({
+    status: 'ok',
+    version: consts.API_VERSION,
+    message: 'Test message -> check github actions and deployment',
+  }),
 );
 
 app.use(`/api/${consts.API_VERSION}`, router);
