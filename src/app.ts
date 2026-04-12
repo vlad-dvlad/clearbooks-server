66b4d26b51f514a650dbf3000b05f313bb200238
Vlad <vlad.ivashko72@gmail.com>
2026-04-12 18:11:58 +0300

    feat: add helth endpoint

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,9 @@ app.use(express.json());
 
 app.use(cors({ origin: config.clientUrl, credentials: true }));
 
+// health check
+app.get('/health', (_, res) => res.json({ status: 'ok', version: consts.API_VERSION }));
+
 app.use(`/api/${consts.API_VERSION}`, router);
 
 export default app;
