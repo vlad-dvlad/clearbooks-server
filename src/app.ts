84507245971fb85dc0f2a793cf6aa72250c83b40
Vlad <vlad.ivashko72@gmail.com>
2026-04-13 23:44:42 +0300

    feat: setup api middlewares

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger';
+import helmet from 'helmet';
 import config, { consts } from './config/config';
 import { router } from './routes/routes';
 import { Routes } from './config/routes';
-import helmet from 'helmet';
+import { errorHandler } from './middlewares/error.middleware';
 
 const app = express();
 app.use(express.json());
@@ -17,6 +18,7 @@ app.use(helmet());
 
 app.use(cors({ origin: config.clientUrl, credentials: true }));
 app.use(Routes.apiDoc, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(errorHandler);
 
 // health check
 app.get('/health', (_, res) =>
